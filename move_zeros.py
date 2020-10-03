b11e11c8342a65c7364663024fe1fe1b6b6dfa51
saurav188 <saurav188@users.github.com>
2020-10-03 12:43:52 +0545

    updating move_zeros.py to make it not change position or other numbers.

--- move_zeros.py
+++ move_zeros.py
@@ -9,7 +9,10 @@ class Solution:
             nums[i],nums[final_index]=nums[final_index],nums[i]
             final_index-=1
         i+=1
-    return nums
+    not_zeros=list(reversed(nums[0:final_index]))
+    not_zeros+=nums[final_index:]
+    for i in range(len(not_zeros)):
+        nums[i]=not_zeros[i]
 
 nums = [0, 0, 0, 2, 0, 1, 3, 4, 0, 0]
 Solution().moveZeros(nums)
