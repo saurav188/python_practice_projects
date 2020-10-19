5ae3acb0a452e07a589a264b84dc623e19c1b2ab
saurav188 <sauravthakur188@gmail.com>
2020-10-19 16:37:56 +0545

    added get_value_in_height_binary_tree.py

---
+++ get_value_in_height_binary_tree.py
@@ -0,0 +1,31 @@
+class Node():
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def valuesAtHeight(root, height):
+    if root==None:
+        return []
+    if height==1:
+        return [root.value]
+    result=[]
+    result+=valuesAtHeight(root.right,height-1)
+    result+=valuesAtHeight(root.left,height-1)
+
+    return result
+
+#     1
+#    / \
+#   2   3
+#  / \   \
+# 4   5   7
+
+a = Node(1)
+a.left = Node(2)
+a.right = Node(3)
+a.left.left = Node(4)
+a.left.right = Node(5)
+a.right.right = Node(7)
+print(valuesAtHeight(a, 3))
+# [4, 5, 7]
