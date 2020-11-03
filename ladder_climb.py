cffe954004f780f8bdbe8ebf9e8aaaabad7f4054
saurav188 <sauravthakur188@gmail.com>
2020-11-03 21:52:40 +0545

    solved ladder_climb.py

---
+++ ladder_climb.py
@@ -0,0 +1,17 @@
+def climb_ladder(no_steps,array_of_steps):
+    steps=[0 for i in range(no_steps+1)]
+    steps[0]=1
+    for i in range(1,no_steps+1):
+        temp=0
+        for j in array_of_steps:
+            if i-j>=0:
+                temp+=steps[i-j]
+        steps[i]=temp
+    return steps[no_steps]
+
+steps=6
+array_of_steps=[1,2]
+print(climb_ladder(steps,array_of_steps))
+
+#return climb
+#1+1+2+3+5+8+13
